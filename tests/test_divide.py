26ecdfda64f2951d01c1d0e94727a6a355aab4d4
Vladimir Pospisil <vladimir.pospisil@evolvsys.cz>
2025-07-22 17:55:37 +0200

    Prejmenovani aplikace

--- tests/test_divide.py
+++ tests/test_divide.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from pdiv import divide
+from divapp import divide
 class TestDivide(TestCase):
     def test_divide_integers(self):
         self.assertEqual(divide(6, 3), 2)
